ec2dd7a3e285b3f9e80e7111908097d5c86e3956
Razvan Crisan <crsn_razvan@yahoo.com>
2019-09-21 17:35:13 +0300

    demo image slider

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,52 @@
-console.log("hello");
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = false; //Set to true for auto slide
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if (current.nextElementSibling) {
+        current.nextElementSibling.classList.add("current");
+    } else {
+        slides[0].classList.add("current");
+    }
+    setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if (current.previousElementSibling) {
+        current.previousElementSibling.classList.add("current");
+    } else {
+        slides[slides.length - 1].classList.add("current");
+    }
+    setTimeout(() => current.classList.remove("current"));
+};
+
+const initEvents = () => {
+    next.addEventListener("click", e => {
+        nextSlide();
+        if (auto) {
+            clearInterval(slideInterval);
+            slideInterval = setInterval(nextSlide, intervalTime);
+        }
+    });
+    prev.addEventListener("click", e => {
+        prevSlide();
+        if (auto) {
+            clearInterval(slideInterval);
+            slideInterval = setInterval(nextSlide, intervalTime);
+        }
+    });
+};
+
+// autoslide
+if (auto) {
+    slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+initEvents();
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ARPIA_website
+
+[Live perview](https://razvanwebdev.github.io/ARPIA_website/index.html);
