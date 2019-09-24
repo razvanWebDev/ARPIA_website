f2bda6d6592bddbddd3971a26837c987f4945cec
Razvan Crisan <crsn_razvan@yahoo.com>
2019-09-24 07:20:19 +0300

    autoslide on github deploy

--- JS/app.js
+++ JS/app.js
@@ -3,8 +3,9 @@ const nav = document.querySelector("header ul");
 const slides = document.querySelectorAll(".slide");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
+
 const autoSlide = true; //Set to true for auto slide
-const intervalTime = 5000;
+const intervalTime = 5000; //slides interval for autoslide
 let slideInterval;
 
 // Toggle navbar on mobile display
@@ -22,7 +23,7 @@ const nextSlide = () => {
     } else {
         slides[0].classList.add("current");
     }
-    setTimeout(() => current.classList.remove("current"));
+    current.classList.remove("current");
 };
 
 const prevSlide = () => {
@@ -33,7 +34,7 @@ const prevSlide = () => {
     } else {
         slides[slides.length - 1].classList.add("current");
     }
-    setTimeout(() => current.classList.remove("current"));
+    current.classList.remove("current");
 };
 
 // Events listeners
@@ -42,15 +43,18 @@ const initEvents = () => {
     hamburger.addEventListener("click", navToggle);
 
     //home page slides
-    if (window.location.pathname == "/index.html") {
-        next.addEventListener("click", e => {
+    if (
+        window.location.pathname == "/" ||
+        window.location.pathname.slice(-10) == "index.html"
+    ) {
+        next.addEventListener("click", () => {
             nextSlide();
             if (autoSlide) {
                 clearInterval(slideInterval);
                 slideInterval = setInterval(nextSlide, intervalTime);
             }
         });
-        prev.addEventListener("click", e => {
+        prev.addEventListener("click", () => {
             prevSlide();
             if (autoSlide) {
                 clearInterval(slideInterval);
