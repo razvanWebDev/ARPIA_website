45d334faacdf2dc48ce3236f183a74f047b3a17e
Razvan Crisan <crsn_razvan@yahoo.com>
2019-12-03 13:16:06 +0200

    customized php comments

--- form.php
+++ form.php
@@ -1,46 +1,15 @@
 <?php 
-// $name = $_POST['name'];
-// $email = $_POST['email'];
-// $message = $_POST['message'];
-// $formcontent="From: $name \n Message: $message";
-// $recipient = "crsn_razvan@yahoo.com";
-// $subject = "Contact Form";
-// $mailheader = "From: $email \r\n";
-// mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-// echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
-// ;
-
-
-
-
-
-
-// if (isset($_POST['submit'])) {
-//     $name = $_POST['name'];
-//     $mailFrom = $_POST['email'];
-//     $phone = $_POST['phone'];
-//     $message = $_POST['message'];
-// }
-
-// $mailTo = "crsn_razvan@yahoo.com";
-// $headers = "From ARPIA website";
-// $txt = "You have received an email from ".$name."\n\n".$message;
-
-// mail($mailTo, $txt, $headers);
-// header("Location: index.html?mailsend")
-
-
 if(isset($_POST['email'])) {
     $email_to = "crsn_razvan@yahoo.com";
-    $email_subject = "website html form submissions";
+    $email_subject = "Mesaj de la website ARPIA - filiala Cluj";
      
      
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />";
+        echo "Ne pare rau, dar am am găsit erori în formular. ";
+        echo "Aceste erori apar mai jos.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors.<br /><br />";
+        echo "Va rugăm sa corectați erorile.<br /><br />";
         die();
     }
      
@@ -49,7 +18,7 @@ if(isset($_POST['email'])) {
         !isset($_POST['email']) ||
         !isset($_POST['phone']) ||
         !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+        died('Ne pare rau, dar am am găsit erori în formular.');       
     }
      
     $name = $_POST['name']; // required
@@ -60,30 +29,30 @@ if(isset($_POST['email'])) {
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    $error_message .= 'Adresa de email nu este valida.<br />';
   }
     $string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    $error_message .= 'Vă rugăm să completați numele.<br />';
   }
  
   if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    $error_message .= 'Vă rugăm să completați mesajul.<br />';
   }
   if(strlen($error_message) > 0) {
     died($error_message);
   }
-    $email_message = "Form details below.\n\n";
+    $email_message = "Detaliile masejului.\n\n";
      
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
      
-    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Nume: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Telefon: ".clean_string($telephone)."\n";
+    $email_message .= "Mesaj: ".clean_string($comments)."\n";
      
      
 // create email headers
@@ -91,10 +60,13 @@ $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 // 'X-Mailer: PHP/' . phpversion();
 mail($email_to, $email_subject, $email_message, $headers);  
-?>
- 
-<!-- place your own success html below -->
 echo "Formularul a fost trimis cu success!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#2a385d;'> Înapoi</a>";
+
+// echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+
+// header("Location: index.html?mailsend")
+?>
+
 <?php
 }
 die();
