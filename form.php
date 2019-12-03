592b1a6ed8e35938c9c366d8626b6977c1f3c79a
Razvan Crisan <crsn_razvan@yahoo.com>
2019-12-03 09:38:20 +0200

    php form

--- form.php
+++ form.php
@@ -10,18 +10,98 @@
 // echo "Thank You!" . " -" . "<a href='contact.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 // ;
 
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
+
+
+
+
+
+// if (isset($_POST['submit'])) {
+//     $name = $_POST['name'];
+//     $mailFrom = $_POST['email'];
+//     $phone = $_POST['phone'];
+//     $message = $_POST['message'];
+// }
+
+// $mailTo = "crsn_razvan@yahoo.com";
+// $headers = "From ARPIA website";
+// $txt = "You have received an email from ".$name."\n\n".$message;
+
+// mail($mailTo, $txt, $headers);
+// header("Location: index.html?mailsend")
+
+
+if(isset($_POST['email'])) {
+     
+    // CHANGE THE TWO LINES BELOW
+    $email_to = "crsn_razvan.com";
+     
+    $email_subject = "website html form submissions";
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['phone']) ||
+        !isset($_POST['message'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+     
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['phone']; // not required
+    $comments = $_POST['message']; // required
+     
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+     
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- place your own success html below -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
 }
-
-$mailTo = "crsn_razvan@yahoo.com";
-$headers = "From ARPIA website";
-$txt = "You have received an email from ".$name."\n\n".$message;
-
-mail($mailTo, $txt, $headers);
-header("Location: index.html?mailsend")
+die();
 
 ?>
